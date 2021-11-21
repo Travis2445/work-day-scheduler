2b78fa9b053fd247b4297d2564f6a2cf455dfa9f
Travis2445 <90297320+Travis2445@users.noreply.github.com>
2021-11-21 12:38:25 -0600

    added the current time and the main function to hold all the logic for the app

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
-// set the current date and display it
-let now = moment().format('dddd, MMMM Do');
-$("#currentDay").text(now);
+// set the current date and format it
+let day = moment().format('dddd, MMMM Do');
+// set the current time and format it
+let timeNow = moment().format('HH:mm a');
+// display current day
+$("#currentDay").text(day);
+// display current time
+$("#currentTime").text(timeNow);
+
+// once the page DOM is ready execute code
+$(function() {
+    console.log("Page is ready!")
+});
