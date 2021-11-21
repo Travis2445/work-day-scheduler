a4288ab5ded1230f0bf7ededfdc7e27bab9572e1
Travis2445 <90297320+Travis2445@users.noreply.github.com>
2021-11-21 14:39:17 -0600

    using getItems, pull data from localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,20 @@ $("#currentTime").text(timeNow);
 
 // once the page DOM is ready execute code
 $(function() {
+
+    // using getItem, pull the saved data from localStorage
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
     
     // capture click on saveBtn
     $(".saveBtn").click(function() {
-        console.log("saveBtn Clicked");
 
         // assign the time of the timeblock and the value of the text area to variables for use in localStorage
         var time = $(this).parent().attr("id");
@@ -25,13 +35,11 @@ $(function() {
     function timeCheck() {
         // assign current time to a var for the timeCheck function
         var actualTime = moment().hour();
-        console.log(actualTime);
 
         // loop thru each of .time-block elements
         $(".time-block").each(function() {
             // using parseInt, parse a a string and return a integer from the id of the .time-block element, 24hr time for simplicity
             var timeBlockTime = parseInt($(this).attr("id"));
-            console.log(timeBlockTime);
 
             // decide which class/color to apply to the time blocks based on the time compared to actualTime
             if (timeBlockTime > actualTime) {
@@ -47,9 +55,8 @@ $(function() {
                 $(this).addClass("present");
                 $(this).removeClass("future");
             }
-        });
-
-        
+        });     
     };
+
     timeCheck();
 });
